f1cb1192bb21e2113ae552a71ea73924c8ef8d31
unknown <victorjames40v@gmail.com>
2023-09-07 06:46:18 +0100

    added the html content, style and functionality

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ This is a welcome project for inters in the hgnx program, which one is requried 
 ## Task
 Frontend page creation with specific elements 
 
-### Ob
+### Objective :
+Develope and host a web page using HTML and CSS that showcase specific personal and real-time data attribuites. Each required element should have a specific data-testid attribuites for easy identification and testing 
---
+++ index.js
@@ -0,0 +1,41 @@
+function UTUtimeUpdate() {
+    const utuTimeContainer = document.querySelector('[data-testid="currentUTCTime"]');
+    const currentUTCTime = new Date().getMilliseconds();
+    utuTimeContainer.innerHTML=`Time: ${currentUTCTime}`
+}
+UTUtimeUpdate()
+
+function currentDay() {
+    const currentDayContainer = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    const currentDay = new Date().getDay()
+    console.log(currentDay)
+    let dayOfTheWeek;
+    
+    switch (currentDay) {
+        case 1:
+            dayOfTheWeek = "Monday"
+            break;
+        case 2:
+            dayOfTheWeek = "Tuesday";
+            break;
+        case 3:
+            dayOfTheWeek = "Wednesday"
+            break;
+        case 4:
+            dayOfTheWeek = "Thursday";
+            break;
+        case 5:
+            dayOfTheWeek = "Friday";
+            break;
+        case 6:
+            dayOfTheWeek = "Saturday";
+            break;
+        case 7:
+            dayOfTheWeek = "Sunday";
+            break;
+        default:
+            dayOfTheWeek;    
+    }
+    currentDayContainer.textContent = dayOfTheWeek;
+}
+currentDay()
